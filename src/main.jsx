f399dab4e70ed5a8d070114fb2f9e006df343005
thivibecode <crwtbdmyxr@privaterelay.appleid.com>
2026-04-30 18:31:18 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ProsumerTool from './ProsumerTool.jsx';
-console.log("rebuild")
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
